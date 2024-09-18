a0973d973d785875cb927d31cd123e1e7eb1a4bb
Diana Ampudia <dianalauraampudia@gmail.com>
2024-09-18 09:43:01 -0600

    pruebas

--- alephart/src/main/java/com/stella/alephart/AlephartApplication.java
+++ alephart/src/main/java/com/stella/alephart/AlephartApplication.java
@@ -2,11 +2,7 @@ package com.stella.alephart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 
 @SpringBootApplication
 public class AlephartApplication {
@@ -15,5 +11,4 @@ public class AlephartApplication {
 		SpringApplication.run(AlephartApplication.class, args);
 	}
 	
-
 }
